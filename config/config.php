0969961c64bba9eb6d58d6b1aadf3071440085e5
Woems Home <0190666666@freenet.de>
2015-12-14 21:03:04 +0100

    Schönheitsfehler korrigieren

--- config/config.php
+++
@@ -1,1 +0,0 @@
-<?php  $CONFIG=array(   "DBSERVER" => "localhost",   "DBDATABASE" => "TerminGrul",   "DBUSER" => "TerminGrul",   "DBPASSWORD" => "J3TqqBadsVuJ2L3N",   "Title" => "TerminGrul"  );  $db = new PDO('mysql:host='.$CONFIG["DBSERVER"].';dbname='.$CONFIG["DBDATABASE"].';charset=utf8', $CONFIG["DBUSER"], $CONFIG["DBPASSWORD"]);?>
